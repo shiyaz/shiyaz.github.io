c828b68b94a008f1bb8a0dc4281db3e3b8d70c40
Shiyas Rasheed <shiyas@induscreed>
2025-06-29 20:41:08 +0000

    feat: expand color palette to 16 distinct colors

--- script.js
+++ script.js
@@ -179,7 +179,7 @@ class LotteryWheel {
 	}
 
 	getSegmentColor(index) {
-		const colors = ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00'];
+		const colors = ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99', '#b15928', '#fbb4ae', '#b3cde3', '#ccebc5', '#decbe4'];
 		return colors[index % colors.length];
 	}
 
